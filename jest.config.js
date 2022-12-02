282af6e6f096b09a4a5ef36c519edd9c83659956
bryan-becker-isi <bryan.becker@isisim.org.br>
2022-12-02 14:01:25 -0300

    feat: impl ts to jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-const nextJest = require("next/jest");
-const createJestConfig = nextJest({
-  dir: "./",
-});
-const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
-module.exports = createJestConfig(customJestConfig);
